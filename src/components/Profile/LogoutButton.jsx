b14607d5cff9880bccc378cc5658cf9659b96014
Ifty Rahman <Ifty09rahman@gmail.com>
2025-12-13 17:27:39 +0600

    add github link

--- src/components/Profile/LogoutButton.jsx
+++ src/components/Profile/LogoutButton.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext.js";
+import { FiLogOut, FiGithub } from "react-icons/fi";
 import "../../css/LogoutButtton.css";
 
 function LogOut() {
@@ -11,10 +12,19 @@ function LogOut() {
     navigate("/");
   }
 
+  function handleGithub() {
+    window.open("https://github.com/Ifty-Rahman/ALTrack", "_blank");
+  }
+
   return (
     <div className="profile">
       <button onClick={handleLogout} className="logout-btn">
+        <FiLogOut className="btn-icon" />
         Logout
+      </button>
+      <button onClick={handleGithub} className="github-btn">
+        <FiGithub className="btn-icon" />
+        Github
       </button>
     </div>
   );
